28256797096e29ae015b23ead6b51225dd26ace1
Damon Bogich <damonbogich@gmail.com>
2021-03-16 12:58:04 -0600

    get by id working

--- api/server.js
+++ api/server.js
@@ -13,11 +13,27 @@ server.get("/", (req, res) => {
 server.get("/api/users", (req, res) => {
     User.find()
     .then(users => {
+        console.log(users)
         res.status(200).json(users)
     })
     .catch(err => {
         res.status(500).send(`${err}`)
     })
 });
+
+// GET	/api/users/:id	Returns the user object with the specified id.
+server.get('/api/users/:id', (req, res) => {
+    let id = req.params.id
+    User.findById(id)
+    .then(user => {
+        if(!user) {
+            res.status(404).json({message: 'user with given id does not exist'})
+        }
+        res.status(200).json(user)
+    })
+    .catch(err => {
+        res.status(500).json({message: err})
+    })
+})
 
 module.exports = server; // EXPORT YOUR SERVER instead of {}
