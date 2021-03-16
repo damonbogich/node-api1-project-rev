cb03e6dddbaf7efd2ce0cd09d7526d743e9d1d6e
Damon Bogich <damonbogich@gmail.com>
2021-03-16 13:35:11 -0600

    mvp met

--- api/server.js
+++ api/server.js
@@ -9,6 +9,23 @@ server.get("/", (req, res) => {
     res.send('hello world');
 });
 
+// POST	/api/users	Creates a user using the information sent inside the request body.
+server.post('/api/users', (req, res) => {
+    const newUser = req.body;
+
+    if(!newUser.name || !newUser.bio) {
+        res.status(400).json({message: 'please provide name and bio for user'})
+    } else {
+        User.insert(newUser)
+        .then(user => {
+            res.status(201).json(user)
+        })
+        .catch(() => {
+            res.status(500).json({message: "Server error"})
+        })
+    }
+});
+
 // GET	/api/users	Returns an array users.
 server.get("/api/users", (req, res) => {
     User.find()
@@ -35,5 +52,42 @@ server.get('/api/users/:id', (req, res) => {
         res.status(500).json({message: err})
     })
 })
+
+// DELETE	/api/users/:id	Removes the user with the specified id and returns the deleted user.\
+server.delete('/api/users/:id', (req, res) => {
+    let id = req.params.id;
+    User.remove(id)
+    .then(user => {
+        if(!user) {
+            res.status(404).json({message: 'id does not exist'})
+        } else {
+            res.status(200).json(user)
+        }
+    })
+    .catch(err => {
+        res.status(500).json({message: err})
+    })
+})
+
+// PUT	/api/users/:id	Updates the user with the specified id using data from the request body. Returns the modified user
+server.put('/api/users/:id', (req, res) => {
+    let id = req.params.id;
+    let newUser = req.body;
+    if(!newUser.name || !newUser.bio) {
+        res.status(400).json({message: 'provide name and bio'})
+    }
+    User.update(id, newUser)
+    .then(user => {
+        if(!user){
+            res.status(404).json({message: 'does not exist'})
+        } else {
+            res.status(200).json(user)
+        }
+    })
+    .catch(() => {
+        res.status(500)
+    })
+
+});
 
 module.exports = server; // EXPORT YOUR SERVER instead of {}
