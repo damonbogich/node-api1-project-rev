4a0a4f6c4730b21ba9eb1eaa3b832fe7147c3727
Damon Bogich <damonbogich@gmail.com>
2021-03-16 12:42:16 -0600

    sets up project and first get working

--- api/server.js
+++ api/server.js
@@ -1,3 +1,23 @@
 // BUILD YOUR SERVER HERE
+const express = require('express');
+const server = express();
+const User = require('./users/model');
 
-module.exports = {}; // EXPORT YOUR SERVER instead of {}
+server.use(express.json());
+
+server.get("/", (req, res) => {
+    res.send('hello world');
+});
+
+// GET	/api/users	Returns an array users.
+server.get("/api/users", (req, res) => {
+    User.find()
+    .then(users => {
+        res.status(200).json(users)
+    })
+    .catch(err => {
+        res.status(500).send(`${err}`)
+    })
+});
+
+module.exports = server; // EXPORT YOUR SERVER instead of {}
